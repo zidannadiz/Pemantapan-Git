7a2df201f447645bcada69f75459026e2633c23a
Muhamad Zidan <zidanlangoet10987@gmail.com>
2024-11-16 16:00:12 +0700

    Gasss

---
+++ switch.js
@@ -0,0 +1,15 @@
+let balonku = "Merah";
+
+switch (balonku) {
+  case "Biru":
+    console.log("Salah Coi");
+    break;
+  case "Merah":
+    console.log("Sukut");
+    break;
+  case "Hitam":
+    console.log("Geger wae");
+    break;
+  default:
+    console.log("Balon tidak diketahui");
+}
